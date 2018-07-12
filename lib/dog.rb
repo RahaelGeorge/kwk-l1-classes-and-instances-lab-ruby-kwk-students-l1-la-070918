4a103c2317b1f6b93dba3804c8612ffd31842933
RahaelGeorge <rahaelg02@gmail.com>
2018-07-12 22:44:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,18 @@
 # dog.rb
 class Dog 
   
+  attr_reader :color, :size, :fur_type
   
+  def initialize(color, size, fur_type)
+    @color = color
+    @size = size
+    @fur_type = fur_type
+  end
   
 end 
+
+fido = Dog.new("brown", "small", "curly")
+snoppy = Dog.new("black", "medium", "straight")
+lassie = Dog.new("golden", "big", "long")
+
+puts fido.color 
